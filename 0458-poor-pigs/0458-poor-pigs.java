c51a01dfe4a2a165b29efa04636259bf9e1c0d83
NotaSimpO <160452816+NotaSimpO@users.noreply.github.com>
2024-02-28 18:13:27 +0700

    Delete 0458-poor-pigs directory

--- 0458-poor-pigs/0458-poor-pigs.java
+++
@@ -1,7 +0,0 @@
-class Solution {
-    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        int turn = (int) Math.floor(minutesToTest / minutesToDie);
-        float b = (float) (Math.log(buckets)/Math.log(turn+1));
-        return (int) Math.ceil(b);
-    }
-}
